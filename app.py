8839a5ca3520acc43f3ff2de88c9a0dd3b61fe31
dev-archyss <confiapaneles@gmail.com>
2026-04-27 11:20:53 -0400

    fixes 2
    cp, inv, ventas

--- app.py
+++ app.py
@@ -1962,3 +1962,4 @@ def get_productos_inventario():
 # ─── ARRANQUE ─────────────────────────────────────────────────────────────
 port = int(os.environ.get('PORT', 5000))
 app.run(debug=False, host='0.0.0.0', port=port)
+
